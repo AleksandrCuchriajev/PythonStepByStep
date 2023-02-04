2429f5619f0418bd217c281211c76f2f32b786f8
AleksandrCuchriajev <info.acoptex@gmail.com>
2023-02-04 10:59:16 +0200

    Initial commit

---
+++ Quizes/Quiz-dictionaries.py
@@ -0,0 +1,23 @@
+# Question 1 Which code defines a dictionary?
+# d= ["Vilnius": "Lithuania", "Berlin": "Germany"]
+# d= ("Vilnius": "Lithuania", "Berlin": "Germany")
+# d= {"Vilnius", "Lithuania", "Berlin", "Germany"}
+# d= {"Vilnius": "Lithuania", "Berlin": "Germany"}
+
+# Question 2 We have defined a dictionary as follows:
+# d = {"Vilnius": "Lithuania", "Berlin": "Germany"}
+# How do we access the first entry (Vilnius / Lithuania)?
+
+# print(d["Vilnius"])
+# print(d["Lithuania"])
+# print(d[0])
+# print(d[1])
+
+# Question 3 We have defined a dictionary as follows:
+# d = {"Vilnius": "Lithuania", "Berlin": "Germany"}
+# How do you check if the entry (Vilnius / Lithuania) is contained in the Dictionary?
+
+# print(0 in d)
+# print("Vilnius" : "Lithuania" in d)
+# print("Vilnius" in d)
+# print("Lithuania" in d)
---
+++ Quizes/Quiz-lists.py
@@ -0,0 +1,11 @@
+# Question 1 Which letter outputs the following program?
+print("Hello Alex"[-2])
+# Question 2 What does the following program output?
+print("Alex Best"[-4:])
+# Question 3 What does the following program output?
+print("Hello World!"[:-2])
+# Question 4 What does the following program output?
+print("Welcome to Acoptex!"[6:-1])
+nums=[1,2,3,4,5].reverse()
+
+print(nums)
