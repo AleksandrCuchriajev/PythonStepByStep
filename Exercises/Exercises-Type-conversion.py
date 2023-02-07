6be1dab733567919ad038de6cde0aeff3c9edcd1
AleksandrCuchriajev <info.acoptex@gmail.com>
2023-02-07 15:38:12 +0200

    Initial commit

---
+++ Exercises/Exercises-Type-conversion.py
@@ -0,0 +1,5 @@
+name = input("Type your name: ").capitalize()
+year = int(input("What year were you born? : "))
+age = 2023-year
+# print("Your name is" + name + " and you are " + age + " years old.")
+print("Your name is {} and you are {} years old.".format(name, age))
