138d2a1e008b852f71a49bb1128cfe8a97fc3378
AleksandrCuchriajev <info.acoptex@gmail.com>
2023-02-07 19:20:20 +0200

    Initial commit

---
+++ Quizes/Quiz-modules.py
@@ -0,0 +1,34 @@
+# Question 1
+# How do you integrate the pandas module? A
+#    A) import pandas
+#    B) pandas.import()
+#    C) import pandas from zoo
+#    D) __import(pandas)__
+
+# Question 2
+# How do you call the read_csv()function from the pandas module after you have included
+# the whole module with the import function? D
+#    A) read_csv(pandas)
+#    B) pandas.pandas.read_csv()
+#    C) pandas: pandas.read_csv()
+#    D) pandas.read_csv()
+
+# Question 3
+# Which command do you need to include the pandas module to use the read_csv()
+# and read_excel() functions directly from the module (like any function in standard python can call)? B
+#    A) from pandas import read_csv(), read_excel()
+#    B) from pandas import read_csv, read_excel
+#    C) import read_csv(), read_excel() from pandas
+#    D) import pandas.read_csv, import pandas.read_excel
+
+# Question 4
+# How can you include the module pandas and rename the handle name so that you
+# can use an abbreviation pan instead of the module name? C
+#    A) import pandas(pan)
+#    B) import pandas, pandas = pan
+#    C) import pandas as pan
+#    D) import pandas.pan
+
+
+
+
