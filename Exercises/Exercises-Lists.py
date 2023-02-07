331d863d688a1d3fecd397ca4be434c0f0917db1
AleksandrCuchriajev <info.acoptex@gmail.com>
2023-02-07 16:09:39 +0200

    Initial commit

---
+++ Exercises/Exercises-Lists.py
@@ -0,0 +1,19 @@
+# What is the output of this code?
+# Guess the output of each print statement!
+new_list = ['a', 'b', 'c']
+print(new_list[1])
+
+print(new_list[-2])
+
+print(new_list[1:3])
+
+new_list[0] = 'm'
+print(new_list)
+
+my_list = [3, 4, 7]
+bonus = my_list + [8]
+my_list[0] = 'a'
+print(my_list)
+
+print(bonus)
+
