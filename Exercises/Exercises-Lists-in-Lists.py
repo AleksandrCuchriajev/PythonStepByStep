38f41c27e30376d2bd227815c1968a4df7fdf5fd
AleksandrCuchriajev <info.acoptex@gmail.com>
2023-02-07 16:26:45 +0200

    Initial commit

---
+++ Exercises/Exercises-Lists-in-Lists.py
@@ -0,0 +1,44 @@
+# using this list:
+basket = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+# access "Oranges" and print it
+# scroll down to see answer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Solution:
+# print(basket[1][1][0])
