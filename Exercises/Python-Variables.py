5177ec7007d45aefe9024c9be35794208130b9af
AleksandrCuchriajev <info.acoptex@gmail.com>
2023-02-10 13:49:43 +0200

    Updated description of Exercise 1

--- Exercises/Python-Variables.py
+++ Exercises/Python-Variables.py
@@ -1,4 +1,6 @@
-# Variables
+# Exercise 1. You have two variables a and b, the value for each received from user input. 
+# Make a program which swap these variables values so the a will get the value of b
+# and b will get the value of a. Print variables in Console.
 
 a = input("a: ")
 b = input("b: ")
