8569dcfb483bdd23831a639128f1f0321c43b093
AleksandrCuchriajev <info.acoptex@gmail.com>
2023-02-11 10:51:40 +0200

    Added Exercise 2

--- Exercises/Exercises-Input-and-Printing.py
+++ Exercises/Exercises-Input-and-Printing.py
@@ -1,6 +1,5 @@
-# Write a program that prints the notes below in Console
-# After you have written your code, you should run your program,
-# and it should print the following:
+# Exercise 1. Write a program that prints the notes below in Console
+# For Example -> Output:
 """
 Python Print Function
 The function is declared like this:
@@ -10,3 +9,11 @@ print('what to print')
 print("Day 1 - Python Print Function")
 print("The function is declared like this:")
 print("print('what to print')")
+
+# Exercise 2. Write a program that prints the number of characters
+# in a user's name. You might need a len function that calculates the length of a string.
+# Your program should work for different inputs, any name that you input.
+# For example
+# Input-> Alex ; Output -> 4
+
+print(len(input("What is your name?")))
