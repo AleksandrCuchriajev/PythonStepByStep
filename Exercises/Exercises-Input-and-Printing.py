04533d0441226cdd03c64f50765239f15a4266dd
AleksandrCuchriajev <info.acoptex@gmail.com>
2023-02-11 10:48:26 +0200

    Initial commit

---
+++ Exercises/Exercises-Input-and-Printing.py
@@ -0,0 +1,12 @@
+# Write a program that prints the notes below in Console
+# After you have written your code, you should run your program,
+# and it should print the following:
+"""
+Python Print Function
+The function is declared like this:
+print('what to print')
+"""
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
