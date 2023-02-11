c8c87dbe64a839dfd7a02570918e4f7ec1dd3464
AleksandrCuchriajev <info.acoptex@gmail.com>
2023-02-11 11:02:07 +0200

    Initial commit

---
+++ Challenges/0-Cocktail-name-generator.py
@@ -0,0 +1,10 @@
+# 1. Create a greeting for your program.
+# 2. Ask the user for the color that they like.
+# 3. Ask the user for the name of a favorite drink.
+# 4. Combine their color and name of their favorite drink and show
+# them their cocktail name.
+# 5. Make sure the input cursor shows on a new line:
+print("Welcome to vacation place name generator")
+color = input("What is your favorite color? \n")
+drink = input("What is your drink name? \n")
+print("The cocktail name is : \n" + color + " " + drink)
