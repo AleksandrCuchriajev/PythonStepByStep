903e2bedbcd9bb6ad738760d97aa47f243d7bffd
AleksandrCuchriajev <info.acoptex@gmail.com>
2023-02-12 14:45:54 +0200

    Initial commit

---
+++ Challenges/5-Picca-order.py
@@ -0,0 +1,33 @@
+"""
+Build an automatic pizza order program.
+Based on a user's order, work out their final bill.
+Small Pizza: $15
+Medium Pizza: $20
+Large Pizza: $25
+Pepperoni for Small Pizza: +$2
+Pepperoni for Medium or Large Pizza: +$3
+Extra cheese for any size pizza: + $1
+
+For Example ->Input
+size = "L"
+add_pepperoni = "Y"
+add_cheese = "N"
+For Example ->Output
+Your final bill is: $28.
+"""
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+add_cheese = input("Do you want extra cheese? Y or N ")
+bill = 0
+if size == "S":
+    bill += 15
+elif size == "M":
+    bill += 20
+else:
+    bill += 25
+if add_pepperoni == "Y":
+        bill += 3
+if add_cheese == "Y":
+        bill += 1
+print(f"Your final bill is: ${bill}")
