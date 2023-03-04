7e10a22200bde490eccbf02584369223dc004759
AleksandrCuchriajev <info.acoptex@gmail.com>
2023-03-04 22:40:11 +0200

    Added exercise 3

--- Exercises/Exercises-set-and-dictionary-comprehentions.py
+++ Exercises/Exercises-set-and-dictionary-comprehentions.py
@@ -4,3 +4,7 @@ print(my_set)
 # Exercise 2. Create set my_set with odd numbers from 0 to 100 squared. Print the set
 my_set = [num ** 2 for num in range(0, 101) if num % 2 != 0]
 print(my_set)
+# Exercise 3. From example_dict create dictionary my_dict with values squared. Print dictionary
+example_dict = {'a': 1, 'b': 2, 'c': 3}
+my_dict = {key: value ** 2 for key, value in example_dict.items()}
+print(my_dict)
