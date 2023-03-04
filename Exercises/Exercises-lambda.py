aaba0d5060427e1209e5dc90280c672f32b02141
AleksandrCuchriajev <info.acoptex@gmail.com>
2023-03-04 22:07:09 +0200

    Added exercise 2

--- Exercises/Exercises-lambda.py
+++ Exercises/Exercises-lambda.py
@@ -1,10 +1,9 @@
-my_list = [1, 2, 3]
-
 # Exercise 1. Write the map() function with lambda
-# def square_2(num):
-#     return num * 2
-#
-#
-# print(list(map(square_2, my_list)))
+my_list = [1, 2, 3]
+print(list(map(lambda num: num ** 2, my_list)))
 
-print(list(map(lambda num: num * 2, my_list)))
+# Exercise 2. Sort the list by second number in the tuple
+my_list = [(0, 2), (4, 3), (9, 9), (10, -1)]
+my_list.sort(key=lambda x: x[1])
+print(my_list)
+
