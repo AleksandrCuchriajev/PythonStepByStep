9b0181cc0dd9b906f6c496ca4d6469704e313912
AleksandrCuchriajev <info.acoptex@gmail.com>
2023-03-16 10:39:40 +0200

    Initial commit

---
+++ Exercises/Exercises-numbers.py
@@ -0,0 +1,4 @@
+# Exercise 1. Write an expression that equals 100. For example 50+50 or 110-10 . 
+# See if you can use more than one arithmetic operator! Write only one expression and submit only one line of code. 
+print(50+50)
+print(110-10)
