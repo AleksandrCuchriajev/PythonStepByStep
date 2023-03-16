108849b42cc8bd4c97d476befb795324d36cae97
Aleksandr Cuchriajev <info.acoptex@gmail.com>
2023-03-17 01:23:25 +0200

    Added Exercise 3

--- Exercises/Exercises-sets.py
+++ Exercises/Exercises-sets.py
@@ -1,10 +1,15 @@
-# Exercise 1. Remove duplicates from list:
+# Exercise 1. Write an expression that would turn the string 'Aleksandr'  into a set of unique letters.
+# For example: set('Square') would return the set {'S', 'q', 'u', 'e', 'r','e'}
+# You should only write one line of code for this. Do not assign a variable name to the set.
+print(set('Aleksandr'))
+
+# Exercise 2. Remove duplicates from list:
 my_list = [1,2,3,5,6,7,7,8,9,9,9]
 my_set = set(my_list)
 my_list_no_duplicates = list(my_set)
 print(my_list_no_duplicates)
 
-# Exercise 2. We have a database of school students:
+# Exercise 3. We have a database of school students:
 school = {'Sam','Randy','Jam','Molly','Dan'}
 # The teachers take attendance and compile it into a list. 
 attendance_list = ['Jam', 'Dan', 'Molly']
