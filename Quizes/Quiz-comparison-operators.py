18885861118e9dc5b781678063d4f3eef43ff8cd
Aleksandr Cuchriajev <info.acoptex@gmail.com>
2023-03-17 01:20:28 +0200

    Initial commit

---
+++ Quizes/Quiz-comparison-operators.py
@@ -0,0 +1,12 @@
+# Question 1. What Boolean will be the output of the following: 2 < 3
+
+# Question 2. What Boolean will be the output of the following:
+# a = 11
+# b = a - 10
+# a > b
+
+# Question 3. What Boolean will be the output of the following: 7 != 7
+
+# Question 4. What Boolean will be the output of the following: 1 < 6 > 8
+
+# Question 5. What Boolean will be the output of the following: 2 <= 4 >= 1
