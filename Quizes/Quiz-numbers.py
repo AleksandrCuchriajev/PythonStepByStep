439bd28fce8922cb4c291f16d27d3b8fbcfb5a57
AleksandrCuchriajev <info.acoptex@gmail.com>
2023-03-16 10:47:23 +0200

    Initial commit

---
+++ Quizes/Quiz-numbers.py
@@ -0,0 +1,20 @@
+# Question 1:
+# Which one of these is a floating point number?
+# 3.2
+# 4
+# 200
+# 125
+
+# Question 2:
+# Which of these will output the result 25?
+# 5**5
+# 5*5
+# 5*6-2
+# 5^5
+
+# Question 3:
+# In Python 3, what is the output of 1/2 ?
+# 0.5
+# 0
+# 1
+
