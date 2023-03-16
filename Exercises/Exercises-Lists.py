ce7eaf5efd8d79b62ef0ff156d05291d87a329d1
AleksandrCuchriajev <info.acoptex@gmail.com>
2023-03-16 12:57:40 -0400

    Added Exercise 5

--- Exercises/Exercises-Lists.py
+++ Exercises/Exercises-Lists.py
@@ -1,8 +1,14 @@
-# Exercise 0. How do you grab 2 from my_list=[1,1,[1,2]]?
+# Exercise 0. Create a list that contains at least one string, one integer and one float.
+# For example: [1, 'two', 3.14159] 
+# Note that the order and number of items doesn't matter. 
+# The answer should just be one list on a single line. Don't assign a variable name to the list.
+[1,2.5,'apple']
+
+# Exercise 1. How do you grab 2 from my_list=[1,1,[1,2]]?
 my_list=[1,1,[1,2]]
 print(my_list[2][1]
-
-# Exercise 1. What is the output of this code?
+      
+# Exercise 2. What is the output of this code?
 # Guess the output of each print statement!
 new_list = ['a', 'b', 'c']
 print(new_list[1])
@@ -21,7 +27,7 @@ print(my_list)
 
 print(bonus)
 
-# Exercise 2. Using this list,
+# Exercise 3. Using this list,
 basket = ["Banana", "Apples", "Oranges", "Blueberries"];
 
 # 1. Remove the Banana from the list
@@ -38,7 +44,7 @@ basket.count("Apples")
 basket.clear()
 print(basket)
 
-# Exercise 3. Fix this code so that it prints a sorted list of all of our friends (alphabetical).
+# Exercise 4. Fix this code so that it prints a sorted list of all of our friends (alphabetical).
 friends = ['Alex', 'Rick', 'John', 'Rendy']
 
 new_friend = ['Sam']
@@ -49,7 +55,7 @@ print(friends.sort() + new_friend)
 # friends.extend(new_friend)
 # print(sorted(friends))
 
-# Exercise 4. You have a list:
+# Exercise 5. You have a list:
 my_list = [1,2,3,4,5,6,7,8,9,10]
 # Count the integers in the list using the loop
 count=0
@@ -57,8 +63,4 @@ for num in my_list:
   count+=num
 print(count)
 
-# Exercise 5. Create a list that contains at least one string, one integer and one float.
-# For example: [1, 'two', 3.14159] 
-# Note that the order and number of items doesn't matter. 
-# The answer should just be one list on a single line. Don't assign a variable name to the list.
-[1,2.5,'apple']
+
