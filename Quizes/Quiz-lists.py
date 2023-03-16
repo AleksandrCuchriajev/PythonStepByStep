07d5f8abf0cceeea2af4d6ac3ef43dcf92c9432d
AleksandrCuchriajev <info.acoptex@gmail.com>
2023-03-16 14:50:51 -0400

    Update Quiz-lists.py

--- Quizes/Quiz-lists.py
+++ Quizes/Quiz-lists.py
@@ -1,26 +1,26 @@
-# Question 1 Which letter outputs the following program?
+# Question 1. Which letter outputs the following program?
 print("Hello Alex"[-2])
 
-# Question 2 What does the following program output?
+# Question 2. What does the following program output?
 print("Alex Best"[-4:])
 
-# Question 3 What does the following program output?
+# Question 3. What does the following program output?
 print("Hello World!"[:-2])
 
-# Question 4 What does the following program output?
+# Question 4. What does the following program output?
 print("Welcome to Acoptex!"[6:-1])
 nums=[1,2,3,4,5].reverse()
 print(nums)
 
-# Question 5: If my_list=[0,1,2] what is the result of my_list.pop()
+# Question 5. If my_list=[0,1,2] what is the result of my_list.pop()
 # 0
 # 1
 # 2
 
-# Question 6: Lists can have multiple object types.
+# Question 6. Lists can have multiple object types.
 # True or False
 
-# Question 7: If my_list=['a','b','c'] What is the result of my_list[1:]?
+# Question 7. If my_list=['a','b','c'] What is the result of my_list[1:]?
 # 'b'
 # ['a','b']
 # ['b','c']
