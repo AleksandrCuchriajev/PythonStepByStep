072ae6bc6d694729b34ccfbf4cef3d92d6cb75b0
AleksandrCuchriajev <info.acoptex@gmail.com>
2023-03-16 11:11:06 +0200

    Initial commit

---
+++ Quizes/Quiz-strings.py
@@ -0,0 +1,12 @@
+# Question 1:
+# Strings are immutable.
+# True or False
+
+# Question 2:
+# If text='hello' what is the output of text[1]
+# 'h'
+# 'l'
+# 'e'
+
+# Question 3:
+# If name='Aleksandr' what is the output of name[2:]?
