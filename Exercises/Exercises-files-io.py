b113931c2ad398906bcea0caf8579ce56a75a481
Aleksandr Cuchriajev <info.acoptex@gmail.com>
2023-03-17 01:36:13 +0200

    Initial commit

---
+++ Exercises/Exercises-files-io.py
@@ -0,0 +1,5 @@
+# Exercise 1. This exercise will require several lines of code.
+# Write a script that opens a file named 'test.txt' , writes 'Hello World'  to the file, then closes it.
+x = open('test.txt', 'w')
+x.write('Hello World')
+x.close()
