c270964608e64d4f555d8b3f9ad87fd94cf6075a
Aleksandr Cuchriajev <info.acoptex@gmail.com>
2023-03-17 10:31:41 +0200

    Added Question 7

--- Quizes/Quiz-dictionaries.py
+++ Quizes/Quiz-dictionaries.py
@@ -1,22 +1,26 @@
 # Question 1. Is this statement True or False? Dictionaries retain order and are a sequence.
 # True or False
 
-# Question 2. Given my_dict={'k1':[1,2,3]} What is the output of my_dict['k1'][1]
+# Question 2. Can you sort a dictionary?
+# Yes
+# No. Because normal dictionaries are mappings not a sequence.
+
+# Question 3. Given my_dict={'k1':[1,2,3]} What is the output of my_dict['k1'][1]
 # 1
 # 2
 # 3
 # None. will be error
 
-# Question 3. Is this statement True or False? Dictionaries are immutable.
+# Question 4. Is this statement True or False? Dictionaries are immutable.
 # True or False
 
-# Question 4. Which code defines a dictionary?
+# Question 5. Which code defines a dictionary?
 # d= ["Vilnius": "Lithuania", "Berlin": "Germany"]
 # d= ("Vilnius": "Lithuania", "Berlin": "Germany")
 # d= {"Vilnius", "Lithuania", "Berlin", "Germany"}
 # d= {"Vilnius": "Lithuania", "Berlin": "Germany"}
 
-# Question 5. We have defined a dictionary as follows:
+# Question 6. We have defined a dictionary as follows:
 # d = {"Vilnius": "Lithuania", "Berlin": "Germany"}
 # How do we access the first entry (Vilnius / Lithuania)?
 # print(d["Vilnius"])
@@ -24,7 +28,7 @@
 # print(d[0])
 # print(d[1])
 
-# Question 6. We have defined a dictionary as follows:
+# Question 7. We have defined a dictionary as follows:
 # d = {"Vilnius": "Lithuania", "Berlin": "Germany"}
 # How do you check if the entry (Vilnius / Lithuania) is contained in the Dictionary?
 # print(0 in d)
