37f9437592c42fd483a443e7e1876d6effcbfad3
Aleksandr Cuchriajev <info.acoptex@gmail.com>
2023-03-17 10:29:13 +0200

    Added Exercise 3

--- Exercises/Exercises-Dictionaries.py
+++ Exercises/Exercises-Dictionaries.py
@@ -30,3 +30,13 @@ user2=user.copy()
 user2['age']=22
 user2['username']="Sandra"
 print(user2)
+
+# Exercise 3. Using keys and indexing, grab the 'hello' from the following dictionaries:
+d = {'simple_key':'hello'}
+print(d['simple_key'])
+d = {'k1':{'k2':'hello'}}
+print(d['k1']['k2'])
+d = {'k1':[{'nest_key':['this is deep',['hello']]}]}
+print(d['k1'][0]['nest_key'][1][0])
+d = {'k1':[1,2,{'k2':['this is tricky',{'tough':[1,2,['hello']]}]}]}
+d['k1'][2]['k2'][1]['tough'][2][0]
