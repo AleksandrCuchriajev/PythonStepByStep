775490f46d38b0d483a8fc8b370e7e0eae94fede
Aleksandr Cuchriajev <info.acoptex@gmail.com>
2023-03-17 11:03:27 +0200

    Added Exercises 4,5,6

--- Exercises/Exercises-exceptions.py
+++ Exercises/Exercises-exceptions.py
@@ -5,9 +5,42 @@ try:
   for i in ['a','b','c']:
     print(i**2)
 except TypeError:
-  print("Type error")
+  print("An error occurred!")
 
-# Exercise 2. Write an exception that catches the error when someone tries to open the following file that does not exist in this directory.
+# Exercise 2. Handle the exception thrown by the code below by using try and except blocks. Then use a finally block to print 'All Done.'
+# x = 5
+# y = 0
+# z = x/y
+try:
+  x = 5
+  y = 0
+  z = x/y
+except ZeroDivisionError:
+  print("You can't divide by zero")
+finally:
+  print('All Done')
+  
+# Exercise 3. Write a function that asks for an integer and prints the square of it. Use a while loop with a try, except, else block to account for incorrect inputs.
+# def ask():
+#    pass
+# ask()
+# Output: Input an integer: null
+# An error occurred! Please try again!
+# Input an integer: 2
+# Thank you, your number squared is:  4
+def ask():    
+    while True:
+        try:
+            user_input = int(input('Input an integer: '))
+        except:
+            print('An error occurred! Please try again!')
+            continue
+        else:
+            break   
+    print('Thank you, your number squared is: ',n**2)
+ask()
+
+# Exercise 4. Write an exception that catches the error when someone tries to open the following file that does not exist in this directory.
 # Output the message "File not found". Open the file with a suitable construct if it does exist in the future - without errors because the file was not closed correctly.
 # file = open("text.txt", mode="r")
 #   print(file)
@@ -17,7 +50,7 @@ try:
 except FileNotFoundError:
   print('File not found')
   
-# Exercise 3. See what type of error occurs when executing the following code. Catch the error with **try except** and output an appropriate error message.
+# Exercise 5. See what type of error occurs when executing the following code. Catch the error with **try except** and output an appropriate error message.
 # names = ["Alex", "Sam", "Red"]
 # scores = {"Alex": 50, "Sam": 100}
 
@@ -43,7 +76,7 @@ try:
 except KeyError:
   print('key is missing')
 
-# Exercise 4. Create an error class that is called if 'names' is an empty dictionary. Then an `EmptyDictionaryError` should occur with the hint that the dictionary 'names' contains no element.
+# Exercise 6. Create an error class that is called if 'names' is an empty dictionary. Then an `EmptyDictionaryError` should occur with the hint that the dictionary 'names' contains no element.
 # names = {}
 # if len(d) == 0:
 #    print("Your error here")
