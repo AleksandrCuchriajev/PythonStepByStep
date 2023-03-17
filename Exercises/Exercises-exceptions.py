5e990f1c260555cc20b8c88901a552f627e589ff
Aleksandr Cuchriajev <info.acoptex@gmail.com>
2023-03-17 10:55:31 +0200

    Added Exercise 4

--- Exercises/Exercises-exceptions.py
+++ Exercises/Exercises-exceptions.py
@@ -1,8 +1,13 @@
-# Exceptions
+# Exercise 1. Handle the exception thrown by the code below by using try and except blocks (TypeError).
+# for i in ['a','b','c']:
+#    print(i**2)
+try:
+  for i in ['a','b','c']:
+    print(i**2)
+except TypeError:
+  print("Type error")
 
-# Exercise 1
-
-# Write an exception that catches the error when someone tries to open the following file that does not exist in this directory.
+# Exercise 2. Write an exception that catches the error when someone tries to open the following file that does not exist in this directory.
 # Output the message "File not found". Open the file with a suitable construct if it does exist in the future - without errors because the file was not closed correctly.
 # file = open("text.txt", mode="r")
 #   print(file)
@@ -12,9 +17,7 @@ try:
 except FileNotFoundError:
   print('File not found')
   
-# Exercise 2
-
-# See what type of error occurs when executing the following code. Catch the error with **try except** and output an appropriate error message.
+# Exercise 3. See what type of error occurs when executing the following code. Catch the error with **try except** and output an appropriate error message.
 # names = ["Alex", "Sam", "Red"]
 # scores = {"Alex": 50, "Sam": 100}
 
@@ -40,9 +43,7 @@ try:
 except KeyError:
   print('key is missing')
 
-# Exercise 3 
-
-# Create an error class that is called if 'names' is an empty dictionary. Then an `EmptyDictionaryError` should occur with the hint that the dictionary 'names' contains no element.
+# Exercise 4. Create an error class that is called if 'names' is an empty dictionary. Then an `EmptyDictionaryError` should occur with the hint that the dictionary 'names' contains no element.
 # names = {}
 # if len(d) == 0:
 #    print("Your error here")
