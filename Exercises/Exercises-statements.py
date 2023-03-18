395b236e9e02b230799e1acfa20d43383989ccf7
Aleksandr Cuchriajev <info.acoptex@gmail.com>
2023-03-18 10:51:50 +0200

    Updated file

--- Exercises/Exercises-statements.py
+++ Exercises/Exercises-statements.py
@@ -8,18 +8,20 @@ for item in my_list:
 # Exercise 2. Use range() to print all the even numbers from 0 to 10.
 for _ in range(0,11,2):
   print(_)
+# or
+print(list(range(0,11,2)))
 
 # Exercises 3. Use a List Comprehension to create a list of all numbers between 1 and 50 that are divisible by 3.
-print([num for num in range(1, 50) if num % 3 == 0])
+print([num for num in range(1, 51) if num % 3 == 0])
 
 # Exercise 4. Go through the sentence below and if the length of a word is even print "even!"
 sentence = 'Print every word in this sentence that has an even number of letters'
 my_list = sentence.split()
 for word in my_list:
     if len(word) % 2 == 0:
-        print('even!')
+        print(word+" <-- has an even length!")
     else:
-        print('odd')
+        print(word+" <-- has an odd length!")
 
 # Exercise 5. Write a program that prints the integers from 1 to 100. But for multiples
 # of three print "Fizz" instead of the number, and for the multiples of five print "Buzz". 
@@ -27,10 +29,12 @@ for word in my_list:
 for num in range(1, 100):
     if num % 5 == 0 and num % 3 == 0:
         print('FizzBuzz', num)
-    if num % 3 == 0:
+    elif num % 3 == 0:
         print('Fizz', num)
-    if num % 5 == 0:
+    elif num % 5 == 0:
         print('Buzz', num)
+    else:
+        print(num)
 
 # Exercise 6. Use List Comprehension to create a list of the first letters of every word in the sentence below:
 sentence = 'Create a list of the first letters of every word in this string'
