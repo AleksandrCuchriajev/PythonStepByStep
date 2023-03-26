79c46ac65cff83572d5840173c94f649763128c2
Aleksandr Cuchriajev <info.acoptex@gmail.com>
2023-03-26 10:31:41 +0300

    Updated Exercise 1

--- Exercises/Exercises-Classes.py
+++ Exercises/Exercises-Classes.py
@@ -1,23 +1,21 @@
 # Exercise 1
 '''
-Given the below class:
-class Cat:
-    species = 'mammal'
+Given the below class:   
+# 1 Instantiate the Friend object with 3 friends
+# 2 Create a function that finds the oldest friend
+# 3 Print out: "The oldest friend is x years old.". x will be the oldest friend age by using the function in #2
+'''
+class Friend:
+    species = 'human'
     def __init__(self, name, age):
         self.name = name
         self.age = age
-   
-# 1 Instantiate the Cat object with 3 cats
-# 2 Create a function that finds the oldest cat
-# 3 Print out: "The oldest cat is x years old.". x will be the oldest cat age by using the function in #2
-'''
+        
+friend1=Friend('Alex',25)
+friend2=Friend('Sonya',30)
+friend3=Friend('John',42)
 
-cat1=Cat('a',2)
-cat2=Cat('b',3)
-cat3=Cat('c',1)
-
-def get_oldest_cat(*args):
+def get_oldest_friend(*args):
     return max(args)
 
-
-print(f"The oldest cat is {get_oldest_cat(cat1.age, cat2.age, cat3.age)} years old.")
+print(f"The oldest friend is {get_oldest_friend(friend1.age, friend2.age, friend3.age)} years old.")
